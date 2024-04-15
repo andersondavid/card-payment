e83b01817e62f85f6f2194be36b84a53ddfe0a93
Anderson David SF <andersondavidprofissional@gmail.com>
2024-04-14 23:20:53 -0300

    feat: add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,104 +36,111 @@ export default function Home() {
 
   return (
     <main className="min-h-screen flex justify-center items-center">
-      <div className="bg-nb-clear rounded-2xl p-8 flex border-4 relative before:border-4 before:absolute before:-z-10 before:top-2 before:left-0 before:bg-nb-dark-blue before:w-full before:h-full before:rounded-2xl">
-        <div>
-          <div className="w-80">
-            <TextInput
-              id="card-number"
-              label="Numero do Cartão"
-              mask={[
-                /\d/,
-                /\d/,
-                /\d/,
-                /\d/,
-                " ",
-                /\d/,
-                /\d/,
-                /\d/,
-                /\d/,
-                " ",
-                /\d/,
-                /\d/,
-                /\d/,
-                /\d/,
-                " ",
-                /\d/,
-                /\d/,
-                /\d/,
-                /\d/,
-              ]}
-              placeholder="0000 0000 0000 0000"
-              value={cardData.cardNumber}
-              onChange={(e) => handleChange("cardNumber", e)}
-            />
-          </div>
-          <div className="w-36 inline-block">
-            <TextInput
-              id="card-validity"
-              label="Validade"
-              mask={[/\d/, /\d/, "/", /\d/, /\d/]}
-              placeholder="12/34"
-              value={cardData.cardValidity}
-              onChange={(e) => handleChange("cardValidity", e)}
-            />
-          </div>
-          <div className="w-36 ml-8 inline-block">
-            <TextInput
-              id="card-cvv"
-              onFocusBlur={cvvOnFocus}
-              label="CVV"
-              mask={[/\d/, /\d/, /\d/]}
-              placeholder="000"
-              value={cardData.cardCvv}
-              onChange={(e) => handleChange("cardCvv", e)}
-            />
-          </div>
-          <div className="w-80">
-            <TextInput
-              id="owner-name"
-              label="Nome no Cartão"
-              placeholder="Seu nome..."
-              value={cardData.ownerName}
-              onChange={(e) => handleChange("ownerName", e)}
-            />
-          </div>
-          <div className="w-80">
-            <TextInput
-              id="owner-cpf"
-              label="CPF do titular"
-              mask={[
-                /\d/,
-                /\d/,
-                /\d/,
-                ".",
-                /\d/,
-                /\d/,
-                /\d/,
-                ".",
-                /\d/,
-                /\d/,
-                /\d/,
-                "-",
-                /\d/,
-                /\d/,
-              ]}
-              placeholder="000.000.000-00"
-              value={cardData.ownerCpf}
-              onChange={(e) => handleChange("ownerCpf", e)}
-            />
-          </div>
+      <div>
+        <div className="py-4 px-8">
+          <h1 className="font-lexend-mega font-bold text-2xl">
+            Adicione um método de pagamento
+          </h1>
         </div>
-        <div className="ml-8 flex flex-col justify-between pt-10">
-          <Card
-            cardNumber={cardData.cardNumber}
-            name={cardData.ownerName}
-            validate={cardData.cardValidity}
-            cvv={cardData.cardCvv}
-            cvvFocus={cvvFocus}
-          />
-          <div className="w-80">
-            <SubmitButton text="Confirmar" />
+        <div className="bg-nb-clear rounded-2xl p-8 pt-4 flex border-4 relative before:border-4 before:absolute before:-z-10 before:top-2 before:left-0 before:bg-nb-dark-blue before:w-full before:h-full before:rounded-2xl">
+          <div>
+            <div className="w-80">
+              <TextInput
+                id="card-number"
+                label="Numero do Cartão"
+                mask={[
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  " ",
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  " ",
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  " ",
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                ]}
+                placeholder="0000 0000 0000 0000"
+                value={cardData.cardNumber}
+                onChange={(e) => handleChange("cardNumber", e)}
+              />
+            </div>
+            <div className="w-36 inline-block">
+              <TextInput
+                id="card-validity"
+                label="Validade"
+                mask={[/\d/, /\d/, "/", /\d/, /\d/]}
+                placeholder="12/34"
+                value={cardData.cardValidity}
+                onChange={(e) => handleChange("cardValidity", e)}
+              />
+            </div>
+            <div className="w-36 ml-8 inline-block">
+              <TextInput
+                id="card-cvv"
+                onFocusBlur={cvvOnFocus}
+                label="CVV"
+                mask={[/\d/, /\d/, /\d/]}
+                placeholder="000"
+                value={cardData.cardCvv}
+                onChange={(e) => handleChange("cardCvv", e)}
+              />
+            </div>
+            <div className="w-80">
+              <TextInput
+                id="owner-name"
+                label="Nome no Cartão"
+                placeholder="Seu nome..."
+                value={cardData.ownerName}
+                onChange={(e) => handleChange("ownerName", e)}
+              />
+            </div>
+            <div className="w-80">
+              <TextInput
+                id="owner-cpf"
+                label="CPF do titular"
+                mask={[
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  ".",
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  ".",
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  "-",
+                  /\d/,
+                  /\d/,
+                ]}
+                placeholder="000.000.000-00"
+                value={cardData.ownerCpf}
+                onChange={(e) => handleChange("ownerCpf", e)}
+              />
+            </div>
+          </div>
+          <div className="ml-8 flex flex-col justify-between pt-10">
+            <Card
+              cardNumber={cardData.cardNumber}
+              name={cardData.ownerName}
+              validate={cardData.cardValidity}
+              cvv={cardData.cardCvv}
+              cvvFocus={cvvFocus}
+            />
+            <div className="w-80">
+              <SubmitButton text="Confirmar" />
+            </div>
           </div>
         </div>
       </div>
